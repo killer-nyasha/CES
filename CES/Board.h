e8ebf61efd6badbf90b9fe0139016f22ad25d4be
Aleksey <37839016+GercogKaban@users.noreply.github.com>
2020-08-10 21:46:17 +0300

    [MINOR]

--- CES/Board.h
+++ CES/Board.h
@@ -1,15 +1,48 @@
 #pragma once
 
 #include <bitset>
+#include <iostream>
 
-template <typename size_t X, size_t Y>
+template <typename const size_t X, const size_t Y>
 class Board
 {
+
 public:
 
-    Board();
+    enum Field
+    {
+        empty,
+        wChecker,
+        bChecker,
+        wKing,
+        bKing,
+    };
+
+    Board()
+    {
+        board.set(0);
+        board.set(2);
+    }
+    
+    size_t getField(const size_t x, const size_t y) const
+    {
+        return getBitsByPos(x, y).to_ullong();
+    }
+
+    size_t setField(const size_t x, const size_t y, const size_t val)
+    {
+
+    }
 
 private:
+
+    std::bitset<3> getBitsByPos(const size_t x, const size_t y)
+    {
+        std::bitset<3> res;
+        for (size_t i = 0; i < 3; ++i)
+            res.set(i, board[X * (x - 1) + 3 * (y - 1) + i]);
+        return res;
+    }
 
     std::bitset<X*Y/2*3> board;
 };
