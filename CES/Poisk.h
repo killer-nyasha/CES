e9a742434394f92832257a5c558432d362212334
killer-nyasha <killer.nyasha.99@gmail.com>
2020-09-19 17:28:14 +0300

    [MINOR] Немного фиксов.

--- CES/Poisk.h
+++ CES/Poisk.h
@@ -54,6 +54,9 @@ public:
 		auto p_turnsW = model.getVariants();
 		auto turnsW = *p_turnsW;
 
+		if (turnsW.size() == 1)
+			return turnsW[0];
+
 		std::vector<float> results;
 
 		for (size_t i = 0; i < turnsW.size(); i++)
@@ -126,6 +129,12 @@ protected:
 			throw std::exception();
 	}
 
+	int getModelTurn(M& model)
+	{
+		auto p_turnsB = model.getVariants();
+		return model.whoseTurn();
+	}
+
 	void setPlayerFromModel(M& model)
 	{
 		if (model.whoseTurn() == White)
@@ -153,11 +162,13 @@ protected:
 		if (model.won() != GAME_GOES_ON)
 		{ 
 			if (model.won() == player)
-				rating += 1;
+				rating += 10000;
+			if (model.won() != player)
+				rating -= 10000;
 		}
 		else
 		{
-			rating += ((player == 1 ? -1 : 1) * model.boardAnalize() + 1) / 2.0f;//0...2 / 2 = 0...1
+			rating += ((player == 1 ? -1 : 1) * model.boardAnalize() + 1) / 2.0f; //0...2 / 2 = 0...1
 		}
 
 		return rating;
@@ -326,30 +337,34 @@ protected:
         short maxRating = INT16_MIN;
 		//float maxRating = 0;
 
+		// = getModelTurn(model);
+
 		if (model.won() == GAME_GOES_ON && depth > 0)
 		{
 			//if (model.whoseTurn() == player)
 			//{
 			auto variants = model.getVariants();
+			int turn = model.whoseTurn();
+
 			for (auto& v : *variants)
 			{
 				M modelCopy = model;
 				modelCopy.doTurn(v);
 				float r = poisk4(modelCopy, depth - 1);
 
-                if (depth % 2 == 0)
+                if (turn != player /*depth % 2 == 0*/)
                 {
                     if (r > maxRating)
                         maxRating = r;
                 }
-
                 else
                 {
                     if (r < minRating)
                         minRating = r;
                 }
+
 			}
-            if (depth % 2 == 0)
+            if (turn != player /*depth % 2 == 0*/)
                 minRating = maxRating;
 		}
 
@@ -412,7 +427,7 @@ protected:
 	//auto p_turnsW;
 
 	//для многопоточного поиска кладём результаты одного из ходов.
-	//используем poisk2
+	//используем poisk4
 	void getTurnResult(size_t i, Turn t, M* model)
 	{
 		M modelCopy = *model;
@@ -460,6 +475,9 @@ public:
 		auto turnsW = *p_turnsW;
 
 		//std::vector<std::thread> threads;
+
+		if (turnsW.size() == 1)
+			return turnsW[0];
 
 		for (size_t i = 1; i < turnsW.size(); i++)
 			static_results[i] = 32000;
